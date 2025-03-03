0ac98eb34d62e71cd4263031bbbc791a6ad25609
leocodes36 <riemer@kth.se>
2025-03-03 14:09:34 +0100

    Add files via upload

--- turbulencebox_jet.py
+++ turbulencebox_jet.py
@@ -1,6 +1,6 @@
 """
 Synthesizing an isotropic turbulence box coherent with IEC 61400-1: 2019, utilising Hipersim.
-Constraining it with a synthetic, Kaimal spectrum-based time series at the beginning.
+Constraining it with a synthetic, Kaimal spectrum-based time series to make a lower level jet.
 
 Leonard Riemer - 07/02/2025
 """
@@ -25,16 +25,28 @@ dtu10mw = loadFromJSON(fp("dtu10mw.json"))  # Includes wind & turbulence class w
 U = dtu10mw["V_ave"]
 TI = dtu10mw["I_ref"]
 
+# Reference point
+yr, zr = 90, 160
+
 # Define simulation paramters
 params = {}
 params["L"] = get_length(dtu10mw["zhub"])                   # Turbulence length [m]
 params["alphaepsilon"] = get_strength()                     # Turbulence strength [m^(4/3)/s^2], default set to 1 and scale timeseries
 params["Gamma"] = get_anisotropy()                          # Anisotropy [-]
-params["Nxyz"] = (1024,120,120)                             # Dimensions of turbulence box
-params["dxyz"] = (2,2,2)                                    # Spacing between points [m]
+params["Nxyz"] = (200,180,180)                              # Dimensions of turbulence box
+params["dxyz"] = (2,1,1)                                    # Spacing between points [m]
 params["seed"] = 1                                          # Seed for random number gen
 params["HighFreqComp"] = 1                                  # Compensation at high frequencies to make sure 5/3 law?
 params["double_xyz"] = (False, True, True)                  # Doubling along given axis for bigger box
+
+# Constraints
+# Turbulence has U=15[m/s] and TI=32% (2 * Iref) (chosen arbritrarily)
+# Jet is 3 [m/s] faster and double the turbulence
+U_turb = U + 3
+TI_turb = 2 * TI
+positions = [(52, 160), (128, 160)]
+y1, z1 = positions[0]
+y2, z2 = positions[1]
 
 # Generate turbulence box
 mtf = MannTurbulenceField.generate(alphaepsilon = params["alphaepsilon"],
@@ -45,9 +57,6 @@ mtf = MannTurbulenceField.generate(alphaepsilon = params["alphaepsilon"],
                                    seed = params["seed"],
                                    HighFreqComp = params["HighFreqComp"],
                                    double_xyz = params["double_xyz"])
-
-# Store turbulence data in variables (uvw correspond to xyz directions)
-u,v,w = mtf.uvw
 
 # Scale box to match the IEC wind and turbulence class
 print (f'Before: Box TI={mtf.uvw[0].std(0).mean()/U:.3f}, alphaepsilon:{mtf.alphaepsilon:.3f}, theoretical spectrum TI {mtf.spectrum_TI(U):.2f}')
@@ -67,6 +76,7 @@ k_lut, spectra_lut = mtf.spectra_lookup()
 c_lst = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728']
 s_lst = ["uu","vv","ww","uw"]
 
+# --- Figure 1 ---
 plt.figure()
 for phi_rea, phi_int, phi_lut, c, l in zip(spectra_rea, spectra_int, spectra_lut, c_lst, s_lst):
     plt.semilogx(k_rea, phi_rea*k_rea, color=c, label=l)
@@ -87,24 +97,31 @@ print("Spectrum Variance", var_uu_spec)
 var_uu_real = mtf.uvw[0].var(0).mean()
 print("Realization Variance", var_uu_real)
 
-# Plot a front view of the turbulence at x = 0 plane
+# --- Figure 2 ---
+# Plot a front view of the turbulence at x = 100 plane
 plt.figure()
-da.sel(uvw='u',x=0).plot(y='y')
+da.sel(uvw='u',x=100).plot(y='z')
 plt.axis('scaled')
+plt.grid()
 
-# Plot a side view of the turbulence at y = 100 plane
+# --- Figure 3 ---
+# Plot a side view of the turbulence at y1 plane before constraint
 plt.figure()
-da.sel(uvw='u',y=100).plot(x='x')
-plt.axis('scaled')
+da.sel(uvw='u',y=y1).plot(x='x')
+#plt.axis('scaled')
+plt.grid()
 
-# Constraints / Plotting a timeseries
-# Turbulence has U=10[m/s] and TI=32% (2 * Iref) (chosen arbritrarily)
-U_turb = U
-TI_turb = 2 * TI
-positions = [(100, 100), (110, 110)]
-y1, z1 = positions[0]
+# --- Figure 4 ---
+# Plot a side view of the turbulence at reference plane before constraint
+plt.figure()
+da.sel(uvw='u', y=yr).plot(x='x')
+#plt.axis('scaled')
+plt.grid()
+
+# Get the constraints data
 constraints = constraint_gen(positions, U_turb, TI_turb, params["Nxyz"], params["dxyz"], params["L"])
 
+# --- Figure 5 ---
 # Plotting "Raw turbulence" before constraining
 axes = plt.subplots(3, 1)[1]
 for ax, c in zip(axes, 'uvw'):
@@ -117,19 +134,49 @@ mtf.constrain(constraints)
 for i, (ax, c) in enumerate(zip(axes, 'uvw'), 3):
     mtf.to_xarray().sel(uvw=c, y=y1, z=z1).plot(ax=ax, label='Constrained turbulence')
 
-    ax.plot([], '.k', label='Contraints')
-    for con in constraints:
-        ax.plot(con[0], con[i], '.k')
-    ax.legend()
+    ax.plot([], '.k', label='Constraints')
+    constraints_filtered = [con for con in constraints if con[1] == y1 and con[2] == z1]
 
-# Plot another front view to see difference
-# I see no difference at (y, z) = (10, 10)??
+    for con in constraints_filtered:
+        ax.plot(con[0], con[i], '.k', markersize=1.5)
+
+da1 = mtf.to_xarray()
+
+# --- Figure 6 ---
+# Plot a front view of the turbulence at x = 100 plane
 plt.figure()
-da.sel(uvw='u',x=0).plot(y='y')
+da1.sel(uvw='u',x=100).plot(y='z')
 plt.axis('scaled')
+plt.grid()
 
+# --- Figure 7 ---
+# Plot a side view of the turbulence at y1 plane
 plt.figure()
-da.sel(uvw='u',y=y1).plot(x='x')
-plt.axis('scaled')
+da1.sel(uvw='u',y=y1).plot(x='x')
+#plt.axis('scaled')
+plt.grid()
 
-make_plots("constr")
+# --- Figure 8 ---
+# Plot a side view of the turbulence at reference plane after constraint
+plt.figure()
+da1.sel(uvw='u',y=yr).plot(x='x')
+#plt.axis('scaled')
+plt.grid()
+
+# --- Figure 9 ---
+# Plot the vertical wind velocity profile in reference plane at x=100 after constraint
+plt.figure()
+da.sel(uvw='u', y=yr, x=100).plot(y='z', label='before constraint')
+da1.sel(uvw='u', y=yr, x=100).plot(y='z', label='after constraint')
+plt.grid()
+plt.legend()
+
+# --- Figure 10 ---
+# Plot the vertical wind velocity profile in y1 plane at x=100 after constraint
+plt.figure()
+da.sel(uvw='u', y=y1, x=100).plot(y='z', label='before constraint')
+da1.sel(uvw='u', y=y1, x=100).plot(y='z', label='after constraint')
+plt.grid()
+plt.legend()
+
+make_plots("jet")
--- turbulencebox_wave_points.py
+++ turbulencebox_wave_points.py
@@ -1,15 +1,18 @@
 """
 Synthesizing an isotropic turbulence box coherent with IEC 61400-1: 2019, utilising Hipersim.
+Constraining it with a synthetic, Kaimal spectrum-based time series at the beginning.
 
-Leonard Riemer - 13/02/2025
+Leonard Riemer - 07/02/2025
 """
 
 # Import relevant libraries
+import os.path
 from hipersim import MannTurbulenceField
 import pylab as plt
 from iec_definitions import *
 from common import *
-import os.path
+from constraint import constraint_wave
+
 
 # Location of input files
 # Shorten the imports
@@ -22,19 +25,24 @@ dtu10mw = loadFromJSON(fp("dtu10mw.json"))  # Includes wind & turbulence class w
 U = dtu10mw["V_ave"]
 TI = dtu10mw["I_ref"]
 
+# Reference point
+yr, zr = 90, 160
+
 # Define simulation paramters
 params = {}
 params["L"] = get_length(dtu10mw["zhub"])                   # Turbulence length [m]
 params["alphaepsilon"] = get_strength()                     # Turbulence strength [m^(4/3)/s^2], default set to 1 and scale timeseries
 params["Gamma"] = get_anisotropy()                          # Anisotropy [-]
-params["Nxyz"] = (200,180,180)                             # Dimensions of turbulence box, for now rather short (4096m correspinding to ca. 400s with U=10m/s)
+params["Nxyz"] = (200,180,180)                             # Dimensions of turbulence box
 params["dxyz"] = (2,1,1)                                    # Spacing between points [m]
 params["seed"] = 1                                          # Seed for random number gen
 params["HighFreqComp"] = 1                                  # Compensation at high frequencies to make sure 5/3 law?
-params["double_xyz"] = (False, False, False)                # Doubling along given axis for bigger box
+params["double_xyz"] = (False, True, True)                  # Doubling along given axis for bigger box
 
-# Reference point
-yr, zr = 90, 160
+# Constraint positions
+positions = [(52, 160), (128, 160)]
+y1, z1 = positions[0]
+y2, z2 = positions[1]
 
 # Generate turbulence box
 mtf = MannTurbulenceField.generate(alphaepsilon = params["alphaepsilon"],
@@ -45,7 +53,6 @@ mtf = MannTurbulenceField.generate(alphaepsilon = params["alphaepsilon"],
                                    seed = params["seed"],
                                    HighFreqComp = params["HighFreqComp"],
                                    double_xyz = params["double_xyz"])
-
 
 # Scale box to match the IEC wind and turbulence class
 print (f'Before: Box TI={mtf.uvw[0].std(0).mean()/U:.3f}, alphaepsilon:{mtf.alphaepsilon:.3f}, theoretical spectrum TI {mtf.spectrum_TI(U):.2f}')
@@ -65,6 +72,7 @@ k_lut, spectra_lut = mtf.spectra_lookup()
 c_lst = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728']
 s_lst = ["uu","vv","ww","uw"]
 
+# --- Figure 1 ---
 plt.figure()
 for phi_rea, phi_int, phi_lut, c, l in zip(spectra_rea, spectra_int, spectra_lut, c_lst, s_lst):
     plt.semilogx(k_rea, phi_rea*k_rea, color=c, label=l)
@@ -85,21 +93,87 @@ print("Spectrum Variance", var_uu_spec)
 var_uu_real = mtf.uvw[0].var(0).mean()
 print("Realization Variance", var_uu_real)
 
-# Plot a front view of the turbulence at x = 0 plane
+# --- Figure 2 ---
+# Plot a front view of the turbulence at x = 100 plane
 plt.figure()
-da.sel(uvw='u',x=0).plot(y='y')
+da.sel(uvw='u',x=100).plot(y='z')
 plt.axis('scaled')
 plt.grid()
 
-# Plot a side view of the turbulence at reference plane
+# --- Figure 3 ---
+# Plot a side view of the turbulence at y1 plane
 plt.figure()
-da.sel(uvw='u',y=yr).plot(x='x')
+da.sel(uvw='u',y=y1).plot(x='x')
 #plt.axis('scaled')
 plt.grid()
 
-# Plot a time series of u at hub height
+# --- Figure 4 ---
+# Plot a side view of the turbulence at reference plane before constraint
 plt.figure()
-da.sel(uvw='u', y=yr, z=90).plot(x='x')
+da.sel(uvw='u', y=yr).plot(x='x')
+#plt.axis('scaled')
 plt.grid()
 
-make_plots("unconstr")
+# Get the constraints data
+# Using theta0 = 290 and dtheta/dz = 0.003 based on Kelly M, Cersosimo RA, Berg J. A universal wind profile for the inversion-capped neutral atmospheric boundary layer. Q J R Meteorol Soc. 2019;145:982–992. https://doi.org/10.1002/qj.3472
+constraints = constraint_wave(positions, U, params["Nxyz"], params["dxyz"], 290, 0.003)
+
+# --- Figure 5 ---
+# Plotting "Raw turbulence" before constraining
+axes = plt.subplots(3, 1)[1]
+for ax, c in zip(axes, 'uvw'):
+    da.sel(uvw=c, y=y1, z=z1).plot(ax=ax, label='Raw turbulence')
+
+# Use .constrain() method of the mtf object
+mtf.constrain(constraints)
+
+# Plot "Constraints" (synthetic, Kaimal spectrum-based time series) and "Constrained turbulence"
+for i, (ax, c) in enumerate(zip(axes, 'uvw'), 3):
+    mtf.to_xarray().sel(uvw=c, y=y1, z=z1).plot(ax=ax, label='Constrained turbulence')
+
+    ax.plot([], '.k', label='Constraints')
+    constraints_filtered = [con for con in constraints if con[1] == y1 and con[2] == z1]
+
+    for con in constraints_filtered:
+        ax.plot(con[0], con[i], '.k', markersize=1.5)
+
+
+da1 = mtf.to_xarray()
+# --- Figure 6 ---
+# Plot a front view of the turbulence at x = 100 plane
+plt.figure()
+da1.sel(uvw='u',x=100).plot(y='z')
+plt.axis('scaled')
+plt.grid()
+
+# --- Figure 7 ---
+# Plot a side view of the turbulence at y1 plane
+plt.figure()
+da1.sel(uvw='u',y=y1).plot(x='x')
+#plt.axis('scaled')
+plt.grid()
+
+# --- Figure 8 ---
+# Plot a side view of the turbulence at reference plane after constraint
+plt.figure()
+da1.sel(uvw='u',y=yr).plot(x='x')
+#plt.axis('scaled')
+plt.grid()
+
+# --- Figure 9 ---
+# Plot a time series at y1, z1 before and after constraint
+plt.figure()
+da.sel(uvw='u', y=y1, z=z1).plot(x='x', label='before constraint')
+da1.sel(uvw='u', y=y1, z=z1).plot(x='x', label='after constraint')
+plt.grid()
+plt.legend()
+
+# --- Figure 10 ---
+# Plot a time series at yr, zr before and after constraint
+plt.figure()
+da.sel(uvw='u', y=yr, z=zr).plot(x='x', label='before constraint')
+da1.sel(uvw='u', y=yr, z=zr).plot(x='x', label='after constraint')
+plt.grid()
+plt.legend()
+
+make_plots("wave_points")
